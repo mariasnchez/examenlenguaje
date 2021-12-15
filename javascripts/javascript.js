b7040c5355f517cc47831deeef3bec143ef8f79f
mariasnchez <91953276+mariasnchez@users.noreply.github.com>
2021-12-15 11:03:41 +0100

    Add files via upload

---
+++ javascripts/javascript.js
@@ -0,0 +1,25 @@
+
+            var acc = document.getElementsByClassName("accordion");
+            var i;
+            
+            for (i = 0; i < acc.length; i++) {
+              acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                  panel.style.display = "none";
+                } else {
+                  panel.style.display = "block";
+                }
+              });
+            }
+
+                function myFunction() {
+                  var x = document.getElementById("myTopnav");
+                  if (x.className === "topnav") {
+                    x.className += " responsive";
+                  } else {
+                    x.className = "topnav";
+                  }
+                }
+ 
